4ad0eab9ef7aa154969c27f25abbe1c9a0a79dd1
Morgan Sziraki <morgan.sziraki@gmail.com>
2023-05-18 00:54:27 +0100

    DEV-001 update server to handle JSON with id, event, data keys

--- server.rb
+++ server.rb
@@ -8,31 +8,34 @@ fifo_path = '/tmp/myfifo'
 File.mkfifo(fifo_path) unless File.exists?(fifo_path)
 connections = []
 
+class Logger
+	def self.info(msg)
+    puts msg
+	end
+
+	def self.error(msg)
+    puts msg
+	end
+end
+logger = Logger.new
+
 Thread.new do
   loop do
     # Wait for data to be available in the named pipe
     fifo = File.open(fifo_path, 'r')
-    message = fifo.gets.chomp
-    j = JSON.parse(message)
+    message = JSON.parse(fifo.gets.chomp)
     fifo.close
 
     begin
-      connections.each do |o|
-        qw(id event data).each { |k| o << "#{k}: #{j[k]}\n" if j.key?(k) }
-        o << "\n"
+      connections.each do |out|
+        out << "id: #{message['id']}\n" if message.key? 'id'
+        out << "event: #{message['event']}\n" if message.key? 'event'
+        out << "data: #{message['data'].to_json}\n\n" if message.key? 'data'
       end
     rescue Exception => e
       logger.error "Error sending SSE stream: #{e.message}"
     end
   end
-end
-
-get '/' do
-  erb :default
-end
-
-get '/help' do
-  erb :help
 end
 
 get '/stream', provides: 'text/event-stream' do
@@ -46,6 +49,11 @@ get '/stream', provides: 'text/event-stream' do
   end
 end
 
-def webpage
-  File.read('/tmp/mydash')
+get '/' do
+  erb :default
 end
+
+get '/help' do
+  erb :help
+end
+
